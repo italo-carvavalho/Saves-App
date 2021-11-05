36b61e92e664a38782931364ebb0a40bf8260e5d
Ivan Teotonio <junioracioli@outlook.com>
2021-11-05 13:34:32 -0300

    Criado a variavel global no arquivo globals.php paralinkar os arquivos com mais facilidade

--- templates/header.php
+++ templates/header.php
@@ -1,10 +1,16 @@
+<?php 
+
+require_once("globals.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css">    
+    <link rel="stylesheet" href="<?=$BASE_URL?>css/styles.css">    
     <title>Prototipo</title>
 </head>
 <body>
