811d2fe83675e3f63cb76fd7a0572090efac5e24
Ênrell <enrellsa10@gmail.com>
2021-11-14 21:11:05 -0300

    home-page-73.3%

--- templates/header.php
+++ templates/header.php
@@ -11,19 +11,16 @@ require_once("globals.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?=$BASE_URL?>css/styles.css">    
-    <title>Prototipo</title>
+    <title>SAVESAPP</title>
 </head>
 <body>
 
    <header>
-   <nav>
-    <div class="header" >
-          <ul>
-              <li id="savesapp"><a href="#">SAVESAPP</a></li>
-              <li class="login-button"><a href="#">Entrar</a></li>
-              <li class="login-button"><a href="#">Cadastrar</a></li>
+        <nav>
+            <a class="logo" href="<?=$BASE_URL?>">SAVESAPP</a>
+            <ul class="navlist">
+                <li><a href="#">Entrar</a></li>
+                <li><a href="#">Cadastro</a></li>
             </ul>
-
-    </div>
-   </nav>
-   </header>
+        </nav>
+    </header>
