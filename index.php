a18ab0a10179592a57ab02e101f1a71e6bb6b0a2
Ênrell <enrellsa10@gmail.com>
2021-11-17 15:39:07 -0300

    home page completa

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 require_once("templates/header.php");
-// require_once("templates/footer.php");
+require_once("templates/footer.php");
 ?>
 	<main>
 		<h1>Faça parte do SAVESAPP</h1>
--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,26 @@
-<footer>    
-         
-            <nav class="redes">
-                <ul>
-                    <li><a href="#" target="_blank"><img src="images/facebook.png" alt="Perfil no Facebook"></a></li>
-                    <li><a href="#" target="_blank"><img src="images/instagram.png" alt="Perfil no Instagram"></a></li>
-                    <li><a href="#" target="_blank"><img src="images/twitter.png" alt="Perfil no Twitter"></a></li>
-               </ul>
-            </nav>
+<footer> 
+        <div class="footer-social">
+            <ul>
+                <li><img class="social-images" src="<?=$BASE_URL?>images/language.png"><a id="idioma" href="#">Idioma</a></li>
+            </ul>
+            <a href="https://www.instagram.com/" target="_blanck"><img class="social-images" src="<?=$BASE_URL?>images/instagram-logo.png"></a>
+            <a href="https://www.twitter.com/" target="_blanck"><img class="social-images" src="<?=$BASE_URL?>images/twitter-logo.png"></a>
+            <a href="https://www.facebook.com/" target="_blanck"><img class="social-images" src="<?=$BASE_URL?>images/facebook-logo.png"></a>
+        </div>
+
+        <div class="footer-ul">
+            <ul>
+                <li><a href="#">Empresa</a></li><br>
+                <li><a href="#">Emprego</a></li>
+                <li><a href="#">Marca</a></li>
+                <li><a href="#">Sobre</a></li>
+            </ul>
+        </div>
+        <div class="footer-ul">
+            <ul>
+                <li><a href="#">Desenvolvedores</a></li>
+                <li><a href="#">Licença</a></li>
+            </ul>
+        </div>
 </footer>
     
