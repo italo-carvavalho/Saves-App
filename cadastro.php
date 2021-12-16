48a40304fc67424528e78a80323ebbcc60b234f7
JoaoPauloSenac <joaoppdsilva@recife.pe.senac.br>
2021-12-15 21:38:59 -0300

    ajuste no css da pagina de cadastro

--- cadastro.php
+++ cadastro.php
@@ -8,10 +8,10 @@ require_once("templates/header.php");
 
 </style>
 
-]
+
     <div class="center">
-        <a class="button " href="<?= $BASE_URL ?>cadastropro.php" style="border:1px solid; padding: 11px 21px; vertical-align: middle; background:#2D888F; color:white;border-radius:6px; font-size: 20px; font-family:helvetica, serif;text-decoration:none;">Profissional</a>
+        <a class="button " href="<?= $BASE_URL ?>cadastropro.php"  id="pro">Profissional</a>
     </div>
     <div class="center">
-        <a class="button floatLeft" href="<?= $BASE_URL ?>cadastrocli.php" style="border:1px solid; padding: 11px 21px; vertical-align: middle; background:#2D888F; color:white;border-radius:6px; font-size: 20px; font-family:helvetica, serif;text-decoration:none;">Cliente</a>
+        <a class="button floatLeft" href="<?= $BASE_URL ?>cadastrocli.php" id="cli">Cliente</a>
     </div>
