5939062c8e9d326b3c5f676c033605feaae080ed
Ênrell <enrellsa10@gmail.com>
2021-12-17 23:03:12 -0300

    removido required /profissional_process

--- cadastropro.php
+++ cadastropro.php
@@ -6,36 +6,36 @@
 
 <div class="row">
     <form class="col s6" action="profissional_process.php" method="POST">
-      <input type="hidden" name="type" value="cadastrar_profissional" required>
+      <input type="hidden" name="type" value="cadastrar_profissional">
     <fieldset>
         <h4>Cadastro do Profissional</h4>
       <div class="">
         <div class="input-field col s12">
-          <input name="nome"  id="full_name" type="text" class="validate" id="name" required>
+          <input name="nome"  id="full_name" type="text" class="validate" id="name">
           <label for="full_name">Nome Completo</label>
         </div>
       </div>
       <div class="">
        <div class="input-field col s12">
-          <input name="telefone" id="icon_telephone" type="telephone" class="validate" id="telefone" required>
+          <input name="telefone" id="icon_telephone" type="telephone" class="validate" id="telefone">
           <label for="icon_telephone">Telefone</label>
         </div>
       </div>    
       <div class="">
         <div class="input-field col s12">
-          <input type="email" id="email" name="email" class="validate" id="email" required> 
+          <input type="email" id="email" name="email" class="validate" id="email" > 
           <label for="email">E-mail</label>
         </div>
        </div>
        <div class="">
       <div class="input-field col s12">
-        <input  name="senha" type="password" class="validate" id="senha" required>
+        <input  name="senha" type="password" class="validate" id="senha">
         <label for="password">Senha</label>
       </div>
     </div>
     <div class="">
       <div class="input-field col s12">
-        <input name="confirmeSenha"  type="password" class="validate" id="confsenha" required>
+        <input name="confirmeSenha"  type="password" class="validate" id="confsenha">
         <label for="password">Confirme sua Senha</label>
      </div>
      </div>
