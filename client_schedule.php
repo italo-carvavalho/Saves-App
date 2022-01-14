ce885366556346c4857aa193d5405da35d79d245
Ênrell <enrellsa10@gmail.com>
2022-01-14 20:37:24 -0300

    atualizando file meus agendamentos

--- client_schedule.php
+++ client_schedule.php
@@ -2,4 +2,13 @@
 require_once("connection.php");
 require_once("globals.php");
 require_once("Model/Message.php");
+require_once("templates/header.php")
+?>
+
+
+
+
+
+<?php
+require_once("templates/footer.php")
 ?>
