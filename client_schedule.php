5f5e28e46d0515d0ba8e23607a9c5c6ac3548d23
Ênrell <enrellsa10@gmail.com>
2022-01-14 21:53:52 -0300

    atualizando código da página de pedidos

--- client_schedule.php
+++ client_schedule.php
@@ -5,9 +5,21 @@ require_once("Model/Message.php");
 require_once("templates/header.php")
 ?>
 
+<?php
+$sql = ("SELECT * FROM users");
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$list = $stmt->fetch(PDO::FETCH_ASSOC)
+if ($list['']) {
+	
+}
+while():
 
+?>
 
+	<li><?php echo $list['name']; ?></li>
 
+<?php endwhile; ?>
 
 <?php
 require_once("templates/footer.php")
