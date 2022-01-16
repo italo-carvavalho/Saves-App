aad2fb7cf5136e5a7b52344d3bcf9844e852c0c0
Ivan Teotonio <ivanteotonio256@gmail.com>
2022-01-16 00:12:50 -0300

    update

--- update_schedule.php
+++ update_schedule.php
@@ -5,15 +5,17 @@ require_once("Model/Message.php");
 
 $message = new Message($BASE_URL);
 
+$fk_id_profesion =  $_SESSION['id_profession'];
+
 $situation = "Em andamento";
 
-$fk_id_user = $_GET['id'];
+$id = $_GET['id'];
 
 try{
-	$sql = "UPDATE schedule SET situation = :situation WHERE fk_id_user = :fk_id_user";
+	$sql = "UPDATE schedule SET situation = :situation WHERE fk_id_profesion = :fk_id_profession";
 	$stmt = $conn->prepare($sql);
 	$stmt->bindParam(":situation",$situation);
-    $stmt->bindParam(":fk_id_user",$fk_id_user);
+    $stmt->bindParam(":fk_id_profession",$);
 	$stmt->execute();
 	$message->setMessage("Solicitação aceita","back");
 
